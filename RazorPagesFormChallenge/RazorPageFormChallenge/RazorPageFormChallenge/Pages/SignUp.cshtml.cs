f05e6eeda7acd15e4a91ed2c890a7300abc0aeab
Ben Thornhill <bthornhill123@gmail.com>
2019-07-02 13:14:49 -0600

    Creating a form using Razor pages

---
+++ RazorPagesFormChallenge/RazorPageFormChallenge/RazorPageFormChallenge/Pages/SignUp.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPageFormChallenge.Models;
+
+namespace RazorPageFormChallenge.Pages
+{
+    public class SignUpModel : PageModel
+    {
+        // This allows you to specify the value of Mode as a query parameter
+        // Ex) https://alsdfd.com/SignUp?Mode=Dark
+        [BindProperty(SupportsGet = true)]
+        public string Mode { get; set; } = "Light";
+
+        [BindProperty]
+        public SignUpDataModel SignUpData { get; set; } = new SignUpDataModel();
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if(ModelState.IsValid == false)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("./ThankYou", new { id = 15 });
+        }
+    }
+}
---
+++ RazorPagesFormChallenge/RazorPageFormChallenge/RazorPageFormChallenge/Pages/ThankYou.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPageFormChallenge.Pages
+{
+    public class ThankYouModel : PageModel
+    {
+        public void OnGet(int id = 0)
+        {
+        }
+    }
+}
