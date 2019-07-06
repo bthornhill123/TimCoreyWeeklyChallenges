68cdc594bfbb7ea00457c018c928e2d8f8af9f46
Ben Thornhill <bthornhill123@gmail.com>
2019-07-05 18:11:30 -0600

    Tim Corey weekly challenge demonstrating the use of generics

---
+++ GenericsChallenge/GenericsChallenge/Address.cs
@@ -0,0 +1,7 @@
+﻿namespace GenericsChallenge
+{
+    public class Address : IHaveTitle {
+        public string Title { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ GenericsChallenge/GenericsChallenge/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericsChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ------- Main Challenge ------
+            List<string> list1 = new List<string>
+            {
+                "Foo",
+                "Fee",
+                "Fough"
+            };
+
+            List<string> list2 = new List<string>
+            {
+                "Fo",
+                "Fgh"
+            };
+
+            var result = MixLists(list1, list2);
+
+            result.ForEach(x => Console.WriteLine(x));
+
+            // ------ Bonus Challenge -------
+            Person p = new Person { Title = "Hey There", Name = "Ben Thornhill" };
+            Address a = new Address { Title = "Mousy", City = "Cleveland" };
+
+            var longerResult = DetermineLongerTitle(p, a);
+
+            Console.WriteLine(longerResult.Title);
+
+            Console.ReadLine();
+        }
+
+        private static List<T> MixLists<T>(List<T> list1, List<T> list2) where T : class
+        {
+            List<T> output = new List<T>();
+            List<T> biggerList;
+            List<T> smallerList;
+
+            if(list1.Count > list2.Count)
+            {
+                biggerList = list1;
+                smallerList = list2;
+            }
+            else
+            {
+                biggerList = list2;
+                smallerList = list1;
+            }
+
+            int smallerCount = smallerList.Count;
+
+            for (int i = 0; i < biggerList.Count; i++)
+            {
+                output.Add(biggerList[i]);
+
+                if(i < smallerCount)
+                {
+                    output.Add(smallerList[i]);
+                }
+            }
+
+            return output;
+        }
+
+        private static IHaveTitle DetermineLongerTitle<T, U>(T item1, U item2) where T : IHaveTitle
+                                                                               where U : IHaveTitle
+        {
+            if(item1.Title.Length > item2.Title.Length)
+            {
+                return item1;
+            }
+            else
+            {
+                return item2;
+            }
+        }
+    }
+}
