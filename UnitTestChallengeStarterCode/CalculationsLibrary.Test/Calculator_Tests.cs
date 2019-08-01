6cac53f0ab91d54dca547d07591e1d58409d1d02
Ben Thornhill <bthornhill123@gmail.com>
2019-08-01 15:21:18 -0600

    Weekly challenge demonstrating unit testing with XUnit. Also demonstrates mocking using Moq to test through and interface so that actual data is not modified.

---
+++ UnitTestChallengeStarterCode/CalculationsLibrary.Test/Calculator_Tests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CalculationsLibrary.Test
+{
+    public class Calculator_Tests
+    {
+        [Theory]
+        [InlineData(5, 4, 9)]
+        [InlineData(8, 3, 11)]
+        [InlineData(1.25, 3.4, 4.65)]
+        public void Add_StandardTest(double x, double y, double expected)
+        {
+            double actual = Calculator.Add(x, y);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(5, 4, 1, 10, 9)]
+        [InlineData(8, 3, 1, 10, 11)]
+        [InlineData(1.25, 3.4, 1, 10, 4.65)]
+        public void LimitedAdd_StandardTest(double x, double y, double min, double max, double expected)
+        {
+            double actual = Calculator.LimitedAdd(x, y, min, max);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(-2, 6, 1, 10, "x")]
+        [InlineData(5, 15, 1, 10, "y")]
+        public void LimitedAdd_OutOfRangeParameters_ShouldThrowException(
+            double x, double y, double min, double max, string badVariable)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(badVariable, () => Calculator.LimitedAdd(x, y, min, max));
+        }
+
+        [Theory]
+        [InlineData(3, 6, .5)]
+        [InlineData(3, 1, 3)]
+        public void Divide_StandardTest(double x, double y, double expeced)
+        {
+            double actual = Calculator.Divide(x, y);
+
+            Assert.Equal(expeced, actual);
+        }
+
+        [Fact]
+        public void Divide_InvalidData_ShouldThrowException()
+        {
+            Assert.Throws<ArgumentException>(() => Calculator.Divide(6, 0));
+        }
+
+    }
+}
---
+++ UnitTestChallengeStarterCode/CalculationsLibrary.Test/TextDataAccess_Tests.cs
@@ -0,0 +1,61 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace CalculationsLibrary.Test
+{
+    public class TextDataAccess_Tests
+    {
+        [Fact]
+        public void SaveText_Normal()
+        {
+            List<string> lines = new List<string>()
+            {
+                "Test one", 
+                "Test two", 
+                "test three"
+            };
+
+            string filePath = @"C:\Temp\Test.txt";
+            string fileName = "Test.txt";
+
+            var mock = new Mock<IWriteToText>();
+            mock.Setup(x => x.WriteToFile(fileName, It.IsAny<List<string>>())).Verifiable();
+
+            TextDataAccess dataAccess = new TextDataAccess();
+            dataAccess.SaveText(filePath, lines, mock.Object);
+
+            mock.Verify();
+        }
+
+        [Fact]
+        public void SaveText_InvalidPath_ShouldThrowException()
+        {
+            List<string> lines = new List<string>()
+            {
+                "Test one",
+                "Test two",
+                "test three"
+            };
+
+            string fileName = @"C:\Temp\Test.txt";
+            string filePath = "Test.txtddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd" +
+                "txtddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd" +
+                "txtddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd" +
+                "txtddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd" +
+                "txtddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd" +
+                "txtddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd";
+
+            var mock = new Mock<IWriteToText>();
+            mock.Setup(x => x.WriteToFile(fileName, It.IsAny<List<string>>())).Verifiable();
+
+            TextDataAccess dataAccess = new TextDataAccess();
+
+            Assert.Throws<PathTooLongException>(
+                () => dataAccess.SaveText(filePath, lines, mock.Object));
+        }
+    }
+}
